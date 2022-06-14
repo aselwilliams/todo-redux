9da006c8232e3ef993b66479b5090c80467b1392
aselwilliams <aselwilliams22@gmail.com>
2022-06-14 15:14:54 -0400

    redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import {useState} from 'react';
+import store from './store'
 import './App.css';
+import * as actions from './actions';
+import {Input, Button} from 'reactstrap'
+import Todo from './Todo'
+import 'bootstrap/dist/css/bootstrap.css';
 
+
+console.log(store.getState(),'store')
 function App() {
+
+  const [inputVal, setInputVal]=useState('')
+
+  const handleAddTodo=()=>{
+    setInputVal('')
+      store.dispatch({
+        type:actions.addTodo,
+        data:{
+          description: inputVal
+        }
+      })
+  }
+  console.log(store.getState())
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className='row'>
+        <div className='col-12 col-sm-6 mt-5 d-flex'>
+          <div>
+          <Input onChange={(e)=>setInputVal(e.target.value)} value={inputVal} style={{width:'590px'}}/>
+          </div>
+          <div className='col-12 col-sm-2'>
+          <Button onClick={handleAddTodo}>Add</Button>
+          </div>
+        </div>
+        <div className='row mt-5'>
+          <div className='col'>
+            <Todo />
+            </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/reducer.js
@@ -0,0 +1,26 @@
+import { addTodo } from "./actions";
+import * as actions from './actions'
+
+function reducer(state=[], action) {
+    if(action.type===actions.addTodo){
+        return [...state,{id:state.length+1,description:action.data.description,completed:false}]
+    } else if(action.type===actions.removeTodo){
+       return state.filter((item)=>item.id!==action.id)
+    } else if( action.type===actions.markCompleteTodo){
+       return state.map((item)=>(item.id===action.id) ? {...item,completed:!item.completed} : item)
+    }
+    return state
+}
+export default reducer
+// function reducer(state, action) {
+//     switch(action.type){
+//         case actions.addTodo:
+//       return [...state,{id:state.length+1,description:action.data.description,completed:false}]
+//          case actions.removeTodo:
+//      return state.filter((item)=>item.id!==action.id)   
+//          case actions.markCompleteTodo:
+//       return state.map((item)=>(item.id===action.id) ? {...item,completed:!item.completed} : item)
+//            default:
+//                return state    
+//     }
+//    }
